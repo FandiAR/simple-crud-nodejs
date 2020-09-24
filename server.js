a69a0f687e84811e7144857261b1707dbf35f1bb
Fandi A Riyanto <fandi@impacto.id>
2020-09-24 12:24:45 +0700

    remove unnecessary codes

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ const posts = require('./routes/posts');
 const server = http.createServer((request, response) => {
   let url = request.url;
   let method = request.method;
-  console.log(method, url);
+  
   switch (method) {
     case 'POST':
-      if (url === '/post') {
         posts.postHandler(request, response);
-      }
       break;
 
     case 'GET':
-      if (url === '/post') {
         posts.getPosts(request, response);
-      }
       break;
 
     case 'PUT':

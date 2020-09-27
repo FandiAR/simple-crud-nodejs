808623e1293b907497714f2a8ce8dce5c8494a54
Fandi A Riyanto <fandi@impacto.id>
2020-09-27 15:30:50 +0700

    splitting code

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
-const http = require('http');
-const posts = require('./routes/posts');
+const express = require('express');
+const app = express();
+const routes = require('./routes');
 
-const server = http.createServer((request, response) => {
-  let url = request.url;
-  let method = request.method;
-  
-  switch (method) {
-    case 'POST':
-        posts.postHandler(request, response);
-      break;
-
-    case 'GET':
-        posts.getPosts(request, response);
-      break;
-
-    case 'PUT':
-      posts.putPosts(request, response);
-      break;
-
-    case 'DELETE':
-      posts.deletePost(request, response);
-      break;
-
-    default:
-      response.writeHead(400, { 'Content-type': 'text/plain' });
-      response.write('Invalid URL');
-      response.end();
-  }
+app.get('/items', (request, response) => {
+  return routes.getItems(request, response);
 });
-server.listen(9000, () => {
+app.get('/item?:id', (request, response) => {
+  return routes.getItem(request, response);
+});
+app.post('/item', (request, response) => {
+  return routes.postItem(request, response);
+});
+app.put('/item?:id', (request, response) => {
+  return routes.putItem(request, response);
+});
+app.delete('/item?:id', (request, response) => {
+  return routes.deleteItem(request, response);
+})
+
+app.listen(9000, () => {
   console.log(`Server running on Port 9000`);
 });
